202917f1b80856444908b6d82a99889b78e282a6
Alex De Luca <alexnaj88@gmail.com>
2022-01-20 20:05:23 -0500

    modal functionality working - need to make it display single targetted recipe and possibly have fade animation

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 'use strict'
 
-document.addEventListener('DOMContentLoaded', addListeners())
+let modal = document.getElementById('recipe-modal')
+window.onclick = function(event) {
+    if (event.target == modal) modal.style.display = "none";
+}
 
-function addListeners() {
-    let recipeElementsArray = Array.from(document.getElementsByClassName('recipe-link'));
-    for(let recipeElement of recipeElementsArray) {
-        recipeElement.addEventListener('mouseover', (e) => {
-            recipeElement.innerText += ' 🥄';
+let recipeElementsArray = Array.from(document.getElementsByClassName('recipe-link'));
+for(let recipeElement of recipeElementsArray) {
+    recipeElement.addEventListener('mouseover', (e) => {
+        recipeElement.innerText += ' 🥄';
 
-        });
-        recipeElement.addEventListener('mouseleave', (e) => {
-            let recipeElementText = recipeElement.innerText.split('');
-            recipeElementText.pop()
-            recipeElementText.pop()
-            recipeElementText.pop()
-            recipeElement.innerText = recipeElementText.join('');
-        });
+    });
+    recipeElement.addEventListener('mouseleave', (e) => {
+        let recipeElementText = recipeElement.innerText.split('');
+        recipeElementText.pop()
+        recipeElementText.pop()
+        recipeElementText.pop()
+        recipeElement.innerText = recipeElementText.join('');
+    });
 
-        recipeElement.addEventListener('click', (e) => {
-
-        });
-    }
+    recipeElement.addEventListener('click', (e) => {
+        modal.style.display = "block";
+        switch (e.target.getAttribute('id')) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                console.log('ERROR - Modal selection NOT valid')
+        }
+    });
 }
